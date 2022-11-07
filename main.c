1a1d275a867691f80d32c0a8a0d10f27f0c7e12f
Elias <dv19enn@gmail.com>
2022-11-07 13:45:59 +0100

    aids

--- main.c
+++ main.c
@@ -47,8 +47,6 @@ typedef struct info
 int found = 0;
 
 sem_t mutex;
-sem_t found_lock;
-sem_t feeder_lock;
 sem_t threads_at_work;
 sem_t empty;
 sem_t full;
@@ -63,90 +61,26 @@ void generate_words(Queue *work_que, int word_length)
     char tmp[word_length + 1];
     memset(tmp, 0, sizeof(char) * word_length + 1);
 
-    long int list_max = INTMAX_MAX;
-    bool list_max_found = false;
-    bool done = false;
+    printf("Producer producing\n");
 
     while (true)
     {
+        char *ret = fgets(tmp, word_length + 1, stdin);
 
-        printf("producer mutex wait\n");
-        sem_wait(&mutex);
-        printf("Producer producing\n");
-        while (!done)
+        if (ret == NULL)
         {
-            // if(list_max_found){
-            //     printf("Producer list_max waiting\n");
-            //     sem_wait(&mutex);
-            // }
-            sem_wait(&found_lock);
-            if (found)
-            {
-                sem_post(&found_lock);
-                printf("PRODUCER exited becaause someone found solution\n");
-                sem_post(&mutex);
-                sem_post(&found_lock);
-                return;
-            }
-            sem_post(&found_lock);
-
-            if (work_que->size >= list_max)
-            {
-                sem_wait(&found_lock);
-                if (found)
-                {
-                    return;
-                }
-                sem_post(&found_lock);
-                sem_post(&mutex);
-                printf("PRODUCER WAITING - MEMORY LOW\n");
-                sem_wait(&memory_mutex);
-                printf("PRODUCER STOP WAIT ---------------------------------------------\n");
-                break;
-            }
-
-            char *ret = fgets(tmp, word_length + 1, stdin);
-
-            if (ret == NULL)
-            {
-                done = true;
-                printf("Producer done!\n");
-                sem_post(&mutex);
-                return;
-            }
-
-            if (tmp[0] == '\n')
-            {
-                continue;
-            }
-
-            double tot_mem = get_phys_pages();
-            double avail_mem = get_avphys_pages();
-            // printf("%f / %f = %f\n", avail_mem, tot_mem, avail_mem / tot_mem);
-
-            tmp[word_length] = '\0';
-
-            // printf("[%s]\n",tmp);
-            queue_enqueue(work_que, tmp);
-            sem_post(&full);
-            if (!list_max_found && (avail_mem / tot_mem) < 0.8)
-            {
-                list_max = work_que->size;
-                list_max_found = true;
-                sem_post(&mutex);
-                printf("PRODUCER WAITING 2 - MEMORY LOW\n");
-                sem_wait(&memory_mutex);
-                printf("PRODUCER STOP WAIT 2 ---------------------------------------------\n");
-                break;
-            }
+            printf("Producer done!\n");
+            return;
         }
-    }
-    sem_post(&mutex);
-}
 
-unsigned long calc_total_problem_size(int word_length, int charset_size)
-{
-    return pow(charset_size, word_length);
+        if (tmp[0] == '\n')
+        {
+            continue;
+        }
+
+        tmp[word_length] = '\0';
+        queue_enqueue(work_que, tmp);
+    }
 }
 
 void *thread_pool(void *arg)
@@ -164,15 +98,12 @@ void *thread_pool(void *arg)
     while (1)
     {
 
-        printf("1 - %u\n", id);
-        sem_wait(&found_lock);
         if (found)
         {
-            sem_post(&found_lock);
             printf("%u exited becaause someone else found solution\n", id);
             return 0;
         }
-        sem_post(&found_lock);
+        
         printf("2 - %u\n", id);
         sem_wait(&fuck_mutex);
         printf("3 - %u\n", id);
@@ -189,16 +120,11 @@ void *thread_pool(void *arg)
         sem_getvalue(&threads_at_work, &threads_working);
 
         int respons = args->no_threads >= no_words ? no_words : no_words / (args->no_threads - threads_working);
-        // if (args->i % 1000 == 0)
-        //{
+
         long int tot_mem = get_phys_pages() * sysconf(_SC_PAGESIZE);
         long int avail_mem = get_avphys_pages() * sysconf(_SC_PAGESIZE);
         printf("QUE = %d - RESPONSE = %d - %f %% klart - MEM %% = %f - id = %u\n", no_words, respons, (100 * (double)args->work_done / args->total_problem_size), 100 * (1.0 - ((double)avail_mem / tot_mem)), id);
-        //}
 
-        // if(respons > 1000000){
-        //     respons = 1000000;
-        // }
 
         if (respons >= queue_size(args->que))
         {
@@ -225,7 +151,6 @@ void *thread_pool(void *arg)
             }
         }
 
-        // unsigned char *words[respons];
         char **words = malloc(sizeof(char *) * respons);
 
         args->work_done += respons;
@@ -233,7 +158,6 @@ void *thread_pool(void *arg)
         {
 
             words[i] = queue_dequeue(args->que);
-            // printf("%s\n", words[i]);
             if (i != 0)
             {
                 sem_wait(&full);
@@ -243,7 +167,6 @@ void *thread_pool(void *arg)
         sem_post(&fuck_mutex);
         sem_post(&mutex);
 
-        // printf("id %u cracking with size %d\n", id, respons);
 
         sem_post(&threads_at_work);
 
@@ -252,12 +175,7 @@ void *thread_pool(void *arg)
 
             if (i > 10000 && i % 100 == 0)
             {
-                int local_found;
-                sem_wait(&found_lock);
-                local_found = found;
-                sem_post(&found_lock);
-
-                if (local_found)
+                if (found)
                 {
                     for (int j = i; j < respons; j++)
                     {
@@ -269,18 +187,18 @@ void *thread_pool(void *arg)
                 }
             }
 
-            unsigned char * digest = md5String(words[i]);
-    
+            unsigned char *digest = md5String(words[i]);
+
             char res[33];
-            for(int i = 0; i < 16; ++i){
-                sprintf(&res[i*2], "%02x", (int)digest[i]);
+            for (int i = 0; i < 16; ++i)
+            {
+                sprintf(&res[i * 2], "%02x", (int)digest[i]);
             }
             free(digest);
 
             if (strncmp(target, res, 32) == 0)
             {
                 args->time = clock() - args->time;
-                sem_wait(&found_lock);
                 args->answer = words[i];
                 found = 1;
                 for (int j = i + 1; j < respons; j++)
@@ -291,7 +209,6 @@ void *thread_pool(void *arg)
                 free(words);
                 for (int i = 0; i < threads_local + 1; i++)
                 {
-                    sem_post(&found_lock);
                     sem_post(&fuck_mutex);
                     sem_post(&full);
                 }
@@ -308,10 +225,7 @@ void *thread_pool(void *arg)
 
 void sig_handler(int signum)
 {
-
-    sem_wait(&found_lock);
     found = 1;
-    sem_post(&found_lock);
     printf("\nInside handler function\n");
     exit(1);
 }
@@ -334,12 +248,10 @@ int main(int argc, char **argv)
         fprintf(stderr, "Cant handle size\n");
         exit(0);
     }
-    // printf("total problem size = %ld\n", total_problem_size);
+    printf("total problem size = %ld\n", total_problem_size);
     int no_threads = atoi(argv[3]);
 
     sem_init(&mutex, 0, 1);
-    sem_init(&found_lock, 0, 1);
-    sem_init(&feeder_lock, 0, 1);
     sem_init(&threads_at_work, 0, 0);
     sem_init(&empty, 0, total_problem_size);
     sem_init(&full, 0, 0);
@@ -359,20 +271,16 @@ int main(int argc, char **argv)
         .work_done = 0,
     };
 
+    generate_words(work_que, word_length);
+    printf("Read words with length %d\n", word_length);
+    printf("---- words created by generator feeder done------------\n");
+
     arguments.time = clock();
 
     for (int i = 0; i < no_threads; i++)
     {
         pthread_create(&cracker_threads[i], NULL, thread_pool, &arguments);
     }
-
-    generate_words(work_que, word_length);
-    printf("Read words with length %d\n", word_length);
-
-    sem_wait(&feeder_lock);
-    arguments.feeder_done = 1;
-    sem_post(&feeder_lock);
-    printf("---- words created by generator feeder done------------\n");
 
     for (int i = 0; i < no_threads; i++)
     {
@@ -381,8 +289,6 @@ int main(int argc, char **argv)
 
     queue_destroy(work_que);
     sem_destroy(&mutex);
-    sem_destroy(&found_lock);
-    sem_destroy(&feeder_lock);
     sem_destroy(&threads_at_work);
     sem_destroy(&fuck_mutex);
     sem_destroy(&memory_mutex);
